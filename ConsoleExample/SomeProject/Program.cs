3c9eac238ae4b8601698692ee47d66a389ffad6d
Yarovyy Ruslan <yarusol@gmail.com>
2016-02-22 10:15:16 +0200

    Initial commit

---
+++ ConsoleExample/SomeProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeProject
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "\n\n\n" );
+			Console.WriteLine( "Hello, Git !" );
+			Console.WriteLine( "\n\n\n" );
+
+		}
+	}
+}
