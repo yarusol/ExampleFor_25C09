823715c07dc1ef177ab51c087fc9d32aeda3bb18
Yarovyy Ruslan <yarusol@gmail.com>
2016-02-22 10:32:30 +0200

    Adds input of integer

--- ConsoleExample/SomeProject/Program.cs
+++ ConsoleExample/SomeProject/Program.cs
@@ -14,6 +14,17 @@ namespace SomeProject
 			Console.WriteLine( "Hello, Git !" );
 			Console.WriteLine( "\n\n\n" );
 
+			int iv;
+			string str;
+			do
+			{
+				Console.WriteLine( "Enter integer value, please : " );
+				str = Console.ReadLine();
+			} while ( ! Int32.TryParse( str, out iv ) );
+			Console.WriteLine( "\n\nYou've entered: {0}. It is cool! Thank you!!!\n\n", iv );
+
+			Console.WriteLine( "\n\n\n" );
+
 		}
 	}
 }
